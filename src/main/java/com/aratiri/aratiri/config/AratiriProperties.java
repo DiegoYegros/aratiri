6f7545cd5c6f95d8dcf2363c06ea4c268be85f4b
Diego Yegros <diego.yegros@somosetica.com>
2025-07-09 08:05:10 -0300

    fix macaroon

--- src/main/java/com/aratiri/aratiri/config/AratiriProperties.java
+++ src/main/java/com/aratiri/aratiri/config/AratiriProperties.java
@@ -1,16 +1,11 @@
 package com.aratiri.aratiri.config;
 
 
-import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Configuration
 @Data
@@ -35,16 +30,4 @@ public class AratiriProperties {
     private String adminMacaroon;
 
     private String lndTlsCert;
-
-    @PostConstruct
-    public void loadProperties() throws IOException {
-        try {
-            adminMacaroon = Files.readString(Path.of(adminMacaroonPath));
-            lndTlsCert = Files.readString(Path.of(lndTlsCertPath));
-
-        } catch (IOException e) {
-            logger.error("Couldn't get macaroon from path [{}]. Exception is: [{}]", adminMacaroonPath, e.getMessage());
-            throw e;
-        }
-    }
 }
--- src/main/java/com/aratiri/aratiri/config/MacaroonCallCredentials.java
+++ src/main/java/com/aratiri/aratiri/config/MacaroonCallCredentials.java
@@ -1,7 +1,14 @@
 package com.aratiri.aratiri.config;
 
+import com.google.common.io.BaseEncoding;
 import io.grpc.CallCredentials;
 import io.grpc.Metadata;
+import io.grpc.Status;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
 
 import java.util.concurrent.Executor;
 
@@ -9,7 +16,16 @@ public class MacaroonCallCredentials extends CallCredentials {
     private final String macaroon;
 
     public MacaroonCallCredentials(String macaroon) {
-        this.macaroon = macaroon;
+        this.macaroonHex = loadMacaroonHex(macaroonPath);
+    }
+
+    private String loadMacaroonHex(String macaroonPath) {
+        try {
+            byte[] macaroonBytes = Files.readAllBytes(Paths.get(macaroonPath));
+            return BaseEncoding.base16().lowerCase().encode(macaroonBytes);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load macaroon file from path: " + macaroonPath, e);
+        }
     }
 
     @Override
