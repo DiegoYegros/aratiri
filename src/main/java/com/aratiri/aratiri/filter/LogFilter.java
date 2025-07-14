4659b2752e218513989f1194d9457ac1e77b174c
Diego Yegros <diegoyegrosa@gmail.com>
2025-07-14 05:34:33 -0300

    LogUtils

---
+++ src/main/java/com/aratiri/aratiri/filter/LogFilter.java
@@ -0,0 +1,40 @@
+package com.aratiri.aratiri.filter;
+
+import com.aratiri.aratiri.utils.LogUtils;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@Component
+@Order(1)
+public class LogFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        logger.info(LogUtils.formatSectionHeader("REQUEST LOG"));
+        logger.info(LogUtils.formatKeyValue("Method", httpRequest.getMethod()));
+        logger.info(LogUtils.formatKeyValue("URI", httpRequest.getRequestURI()));
+        logger.info(LogUtils.formatKeyValue("Query String", httpRequest.getQueryString()));
+        logger.info(LogUtils.formatKeyValue("Content Type", httpRequest.getContentType()));
+        logger.info(LogUtils.formatKeyValue("Headers", ""));
+        Enumeration<String> headerNames = httpRequest.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            logger.info(LogUtils.formatKeyValue("  " + headerName, httpRequest.getHeader(headerName)));
+        }
+        logger.info(LogUtils.formatSectionHeader("END REQUEST LOG", "="));
+        chain.doFilter(request, response);
+    }
+}
--- src/main/java/com/aratiri/aratiri/interceptor/GrpcLoggingInterceptor.java
+++ src/main/java/com/aratiri/aratiri/interceptor/GrpcLoggingInterceptor.java
@@ -1,5 +1,6 @@
 package com.aratiri.aratiri.interceptor;
 
+import com.aratiri.aratiri.utils.LogUtils;
 import io.grpc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,33 +15,33 @@ public class GrpcLoggingInterceptor implements ClientInterceptor {
             CallOptions callOptions,
             Channel next) {
 
-        log.info("==================== START gRPC CALL ====================");
-        log.info("METHOD                 : {}", method.getFullMethodName());
+        log.info(LogUtils.formatSectionHeader("START gRPC CALL"));
+        log.info(LogUtils.formatKeyValue("METHOD", method.getFullMethodName()));
 
         return new ForwardingClientCall.SimpleForwardingClientCall<>(next.newCall(method, callOptions)) {
 
             @Override
             public void start(Listener<RespT> responseListener, Metadata headers) {
-                log.info("METADATA (HEADERS) SENT:");
+                log.info(LogUtils.formatKeyValue("METADATA (HEADERS) SENT", ""));
                 headers.keys().forEach(key -> {
-                    log.info("  {}: {}", key, headers.get(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER)));
+                    log.info(LogUtils.formatKeyValue("  " + key, headers.get(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER))));
                 });
 
                 Listener<RespT> forwardingListener = new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(responseListener) {
                     @Override
                     public void onMessage(RespT message) {
-                        log.info("SERVER'S RESPONSE:\n{}", message.toString());
+                        log.info(LogUtils.formatKeyValue("SERVER'S RESPONSE", "\n" + message.toString()));
                         super.onMessage(message);
                     }
 
                     @Override
                     public void onClose(Status status, Metadata trailers) {
                         if (!status.isOk()) {
-                            log.error("gRPC CALL CLOSED WITH ERROR: {}", status);
+                            log.error(LogUtils.formatKeyValue("gRPC CALL CLOSED WITH ERROR", status));
                         } else {
-                            log.info("gRPC CALL SUCCESSFULLY CLOSED");
+                            log.info(LogUtils.formatKeyValue("gRPC CALL STATUS", "SUCCESSFULLY CLOSED"));
                         }
-                        log.info("==================== END gRPC CALL ====================");
+                        log.info(LogUtils.formatSectionHeader("END gRPC CALL"));
                         super.onClose(status, trailers);
                     }
                 };
@@ -50,7 +51,7 @@ public class GrpcLoggingInterceptor implements ClientInterceptor {
 
             @Override
             public void sendMessage(ReqT message) {
-                log.info("MESSAGE SENT TO SERVER :\n{}", message.toString());
+                log.info(LogUtils.formatKeyValue("MESSAGE SENT TO SERVER", "\n" + message.toString()));
                 super.sendMessage(message);
             }
         };
