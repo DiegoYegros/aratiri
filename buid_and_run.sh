299c092ced79ccf8f29b66a6774e9affa18d4e3a
Diego Yegros <diegoyegrosa@gmail.com>
2025-09-20 18:22:07 -0300

    Removed build_and_run script

--- buid_and_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Running mvn clean install"
-mvn clean install || { echo "Maven clean install failed"; exit 1; }
-
-echo "Building and Running Docker image"
-docker-compose up --build
