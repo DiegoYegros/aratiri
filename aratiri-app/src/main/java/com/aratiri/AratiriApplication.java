6587025f91f4b649ce38063aafc132fb7b417892
Diego Yegros <diegoyegrosa@gmail.com>
2025-10-21 23:56:41 -0300

    added public back to main method

--- aratiri-app/src/main/java/com/aratiri/AratiriApplication.java
+++ aratiri-app/src/main/java/com/aratiri/AratiriApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class AratiriApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(AratiriApplication.class, args);
     }
 
